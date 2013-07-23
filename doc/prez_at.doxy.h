717d20d8310d83187a74bac108e98ae640ea4574
Jerome Forissier <jerome@taodyne.com>
2013-07-23 15:00:57 +0200

    Merge branch 'master' of io.taodyne.com:~soulisse/Work/tao

--- doc/prez_at.doxy.h
+++ doc/prez_at.doxy.h
@@ -1,15 +1,15 @@
 /**
  * @~english
  * @taomoduledescription{PrezAt, prez.at interface}
- *
- * Connect to your NodeJS-enabled presentations through http://prez.at/
+ * <tt>import PrezAt</tt> - Connect to your NodeJS-enabled presentations
+ * through http://prez.at/.@n
  *
  * @endtaomoduledescription{PrezAt}
  *
  * @~french
  * @taomoduledescription{PrezAt, Interface prez.at}
- *
- * Connectez-vous à vos présentations utilisant NodeJS par l'intermédiaire de http://prez.at/ 
+ * <tt>import PrezAt</tt> - Connectez-vous à vos présentations utilisant
+ * NodeJS par l'intermédiaire de http://prez.at/.@n
  *
  * @endtaomoduledescription{PrezAt}
  *
